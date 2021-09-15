9b23a0a34f5bfb7b2d82672e96c7f21e3bbfcceb
nicoSix <nicolasix@outlook.fr>
2021-09-15 16:51:52 +0200

    Updating content grabbing for latest version of DE

--- background.js
+++ background.js
@@ -1,63 +1,62 @@
 function triggerHowrare() {
   if (window.location.href.normalize().includes("https://digitaleyes.market/collections/".normalize())) {
     function displayHowrare() {
-      var collection = window.location.href.split('/')[4]
       items = Array.prototype.slice.call(document.querySelectorAll('p'))
         .filter(domElt => /\d/.test(domElt.innerText))
-        console.log(collection)
-        if (collection == "SolBear") {
-          const url = chrome.runtime.getURL('/rarity/solbears.json');
-    
-          fetch(url)
-              .then((response) => response.json()) //assuming file contains json
-              .then((solbears) => {
-                items.forEach(domItem => {
-                  if (!domItem.isRarityDisplayed) {
-                    itemId = domItem.innerText.replace(/[^0-9]/g,'');
-                    domItem.innerText = domItem.innerText + " (rarity: " + solbears[itemId] + ")";
-                    domItem.isRarityDisplayed = true;
-                }
-            })
-          });
-        } else if (collection == "Rox") {
-          const url = chrome.runtime.getURL('/rarity/rox.json');
-    
-          fetch(url)
-              .then((response) => response.json()) //assuming file contains json
-              .then((roxes) => {
-                items.forEach(domItem => {
-                  if (!domItem.isRarityDisplayed) {
-                    itemId = parseInt(domItem.innerText.replace(/[^0-9]/g,''));
-                    if (roxes[itemId]) {
-                      domItem.innerText = domItem.innerText + " (rarity: " + roxes[itemId] + ")";
-                    } else {
-                      domItem.innerText = domItem.innerText + " (rarity: random)";
-                    }
-                    domItem.isRarityDisplayed = true;
-                }
-            })
-          });
-        } else if (collection == "Solana%20Souls") {
-          const url = chrome.runtime.getURL('/rarity/solsouls.json');
 
-          fetch(url)
-              .then((response) => response.json()) //assuming file contains json
-              .then((souls) => {
-                items.forEach(domItem => {
-                  if (!domItem.isRarityDisplayed) {
-                    itemName = domItem.innerText;
-                    if (souls[itemName]) {
-                      domItem.innerText = domItem.innerText + " (rarity: " + souls[itemName] + ")";
-                    } else {
-                      domItem.innerText = domItem.innerText + " (rarity: random)";
-                    }
-                    domItem.isRarityDisplayed = true;
-                }
-            })
-          });
-        } else {
-          console.error('Collection has not been added yet.')
-        }
+      if (window.location.href.includes("SolBear")) {
+        const url = chrome.runtime.getURL('/rarity/solbears.json');
+  
+        fetch(url)
+            .then((response) => response.json()) //assuming file contains json
+            .then((solbears) => {
+              items.forEach(domItem => {
+                if (!domItem.isRarityDisplayed && domItem.innerText.includes('#')) {
+                  itemId = domItem.innerText.replace(/[^0-9]/g,'');
+                  domItem.innerText = domItem.innerText + " (rarity: " + solbears[itemId] + ")";
+                  domItem.isRarityDisplayed = true;
+              }
+          })
+        });
+      } else if (window.location.href.includes("Rox")) {
+        const url = chrome.runtime.getURL('/rarity/rox.json');
+  
+        fetch(url)
+            .then((response) => response.json()) //assuming file contains json
+            .then((roxes) => {
+              items.forEach(domItem => {
+                if (!domItem.isRarityDisplayed && domItem.innerText.includes('#')) {
+                  itemId = parseInt(domItem.innerText.replace(/[^0-9]/g,''));
+                  if (roxes[itemId]) {
+                    domItem.innerText = domItem.innerText + " (rarity: " + roxes[itemId] + ")";
+                  } else {
+                    domItem.innerText = domItem.innerText + " (rarity: random)";
+                  }
+                  domItem.isRarityDisplayed = true;
+              }
+          })
+        });
+      } else if (window.location.href.includes("Solana%20Souls")) {
+        const url = chrome.runtime.getURL('/rarity/solsouls.json');
+
+        fetch(url)
+            .then((response) => response.json()) //assuming file contains json
+            .then((souls) => {
+              items.forEach(domItem => {
+                if (!domItem.isRarityDisplayed && domItem.innerText.includes('#')) {
+                  itemName = domItem.innerText;
+                  if (souls[itemName]) {
+                    domItem.innerText = domItem.innerText + " (rarity: " + souls[itemName] + ")";
+                  } else {
+                    domItem.innerText = domItem.innerText + " (rarity: random)";
+                  }
+                  domItem.isRarityDisplayed = true;
+              }
+          })
+        });
+      } else {
+        console.error('Collection has not been added yet.')
+      }
     }
 
     window.addEventListener('scroll', function(e) {
